691e78b7e8155c5de796fdfc28a10c2ba6729b90
Simon van der Veldt <simon.vanderveldt@gmail.com>
2014-10-30 21:50:10 +0100

    Create and start new container from image

--- dockerfiles/consul-python/unblocker.py
+++ dockerfiles/consul-python/unblocker.py
@@ -3,7 +3,9 @@
 import sys
 import json
 import base64
-import docker
+from docker import Client
+
+c = Client(base_url='unix://var/run/docker.sock')
 
 for line in sys.stdin:
     print line
@@ -13,7 +15,10 @@ for line in sys.stdin:
         continue
 
     if message:
-    	for image in message:
-			print image["Name"] + ":" + base64.b64decode(image["Payload"])
-			print json.loads(base64.b64decode(image["Payload"]))
+        for image in message:
+            #print image["Name"] + ":" + base64.b64decode(image["Payload"])
+            imagejson = json.loads(base64.b64decode(image["Payload"]))
 
+            container = c.create_container(imagejson["name"] + ":" + str(imagejson["version"]), command='/bin/sleep 30')
+            response = c.start(container=container.get('Id'))
+            print response
